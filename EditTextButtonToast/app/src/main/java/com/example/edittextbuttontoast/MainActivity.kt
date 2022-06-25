db7036519e803bc552f8e305bf9498239b431129
Anmol-Dhiman <anmoldhiman7111@gmail.com>
2022-06-25 10:48:27 +0530

    editText, buttons, snackbar

--- EditTextButtonToast/app/src/main/java/com/example/edittextbuttontoast/MainActivity.kt
+++ EditTextButtonToast/app/src/main/java/com/example/edittextbuttontoast/MainActivity.kt
@@ -3,33 +3,69 @@ package com.example.edittextbuttontoast
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.edittextbuttontoast.ui.theme.EditTextButtonToastTheme
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
+
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Ui(modifier: Modifier = Modifier) {
+
+    val scaffoldState = rememberScaffoldState()
+    var text by remember { mutableStateOf(value = "") }
+    val scope = rememberCoroutineScope()
+
+//    scaffold is used to set the snackBar as the snackBar show on the parentTop and to get control over the positioning we use scaffold
+    Scaffold(modifier = modifier.fillMaxSize(), scaffoldState = scaffoldState) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 30.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            TextField(
+                value = text,
+                label = {
+                    Text(text = "Enter your name")
+                },
+                onValueChange = { text = it },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {
+                scope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar(message = "Hello $text")
+                }
+            }) {
+                Text(text = "Greet me")
+            }
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    EditTextButtonToastTheme {
-        Greeting("Android")
-    }
+    Ui()
 }
