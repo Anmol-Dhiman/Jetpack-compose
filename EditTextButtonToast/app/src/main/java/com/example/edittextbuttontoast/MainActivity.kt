6983d98b0441ecb6f1eb0be58a9bceed403bb7a7
Anmol-Dhiman <anmoldhiman7111@gmail.com>
2022-06-25 11:07:56 +0530

    lazy loading

--- EditTextButtonToast/app/src/main/java/com/example/edittextbuttontoast/MainActivity.kt
+++ EditTextButtonToast/app/src/main/java/com/example/edittextbuttontoast/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 
 
@@ -18,12 +23,58 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-
+            ScrollableListLazyColumn()
         }
     }
 }
 
+
+@Composable
+fun ScrollableListLazyColumn(modifier: Modifier = Modifier) {
+
+//    this will load the text only when it need to be shown in the device
+//    but if we use simple column and for loop then it will generate the whole text field and
+//    which is not efficient for the memory
+    LazyColumn(modifier = modifier.fillMaxSize()) {
+        items(1000) {
+            Text(
+                text = "item $it",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 30.dp),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold, textAlign = TextAlign.Center,
+
+                )
+        }
+    }
+}
+
+
+@Composable
+fun DifferentInputsInLazyColumn(modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier.fillMaxSize()) {
+        itemsIndexed(
+            listOf(
+                "Ronak",
+                "Shekhu",
+                "Vaibhav",
+                "Rajat",
+                "Nishant",
+                "Quatri"
+            )
+        ) { index, string ->
+            Text(
+                text = "$string $index",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 30.dp),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold, textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
 
 @Composable
 fun Ui(modifier: Modifier = Modifier) {
@@ -67,5 +118,5 @@ fun Ui(modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Ui()
+    ScrollableListLazyColumn()
 }
